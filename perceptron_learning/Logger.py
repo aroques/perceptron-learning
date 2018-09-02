d069313b0f52a990e6edc122458be7e553920c8b
Alec Roques <alec.roques@gmail.com>
2018-09-02 18:11:40 -0500

    Add formatting to print_statistics()

--- perceptron_learning/Logger.py
+++ perceptron_learning/Logger.py
@@ -11,8 +11,10 @@ class Logger:
         self.num_vector_updates = 0
 
     def print_statistics(self, w_target, w_hypothesis):
-        print('target function: y = {0:.2f}x + {1:.2f}'.format(tdv.get_slope(w_target), tdv.get_y_intercept(w_target)))
-        print('hypothesis : y = {0:.2f}x + {1:.2f}'.format(tdv.get_slope(w_hypothesis), tdv.get_y_intercept(w_hypothesis)))
-        print('num iterations: ', self.num_iterations)
-        print('num weight vector updates: ', self.num_vector_updates)
+        print('{:24s}: y = {:.2f}x + {:.2f}'.format('target function',
+                                                    tdv.get_slope(w_target), tdv.get_y_intercept(w_target)))
+        print('{:24s}: y = {:.2f}x + {:.2f}'.format('hypothesis',
+                                                    tdv.get_slope(w_hypothesis), tdv.get_y_intercept(w_hypothesis)))
+        print('{:24s}: {:}'.format('number of iterations', self.num_iterations))
+        print('{:24s}: {:}'.format('number of vector updates', self.num_vector_updates))
         # need final mis-classification error
